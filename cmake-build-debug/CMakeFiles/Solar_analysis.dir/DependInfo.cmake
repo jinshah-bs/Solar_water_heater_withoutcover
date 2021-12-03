003466f8e4c98ee90382108d498e21ad45838a84
thinkcomputational <411117005@nitt.edu>
2021-12-03 21:30:58 +0530

    Almost cleared..

---
+++ cmake-build-debug/CMakeFiles/Solar_analysis.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/HTC.cpp" "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/cmake-build-debug/CMakeFiles/Solar_analysis.dir/HTC.cpp.o"
+  "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/TDMA.cpp" "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/cmake-build-debug/CMakeFiles/Solar_analysis.dir/TDMA.cpp.o"
+  "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/Write_data.cpp" "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/cmake-build-debug/CMakeFiles/Solar_analysis.dir/Write_data.cpp.o"
+  "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/main.cpp" "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/cmake-build-debug/CMakeFiles/Solar_analysis.dir/main.cpp.o"
+  "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/mat_prop.cpp" "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/cmake-build-debug/CMakeFiles/Solar_analysis.dir/mat_prop.cpp.o"
+  "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/read_data.cpp" "/home/thinkcomputational/CLionProjects/Solar_water_heater_withoutcover/cmake-build-debug/CMakeFiles/Solar_analysis.dir/read_data.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
